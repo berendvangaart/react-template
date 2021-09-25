13c30465a0e1b75aae87885d6734ae005b9ffb7a
Berend <berendvangaart@gmail.com>
2021-09-25 02:15:03 +0200

    implement Redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import {useDispatch, useSelector} from "react-redux";
+import {decreaseCounter, increaseCounter} from "./Store/Counter/counter.actions";
+
 function App() {
+
+  const dispatch = useDispatch()
+  const state = useSelector((state) => state.counter)
+
   return (
     <div>
-      <h1>hello template</h1>
+      <h1>{state.count}</h1>
+        <button onClick={() => dispatch(increaseCounter(10))}>Increase Count</button>
+        <button onClick={() => dispatch(decreaseCounter(10))}>Decrease Count</button>
     </div>
   );
 }
